2471612e71c95a9f8ec635b7910eaa8de13e56fe
medievalrain <dev@vlad.ph>
2026-03-25 03:21:25 +0300

    Add sonda

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Sonda from "sonda/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	build: { sourcemap: true },
 	plugins: [
 		react(),
 		babel({
 			presets: [reactCompilerPreset()],
 		}),
 		tailwindcss(),
+		Sonda(),
 	],
 });
