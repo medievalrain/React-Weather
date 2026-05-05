2ca8da6f19b503a00059229154b6dfe29f8061e1
medievalrain <dev@vlad.ph>
2026-05-05 19:18:05 +0300

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import Sonda from "sonda/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	build: { sourcemap: true },
